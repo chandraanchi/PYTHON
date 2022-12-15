9127ca3c119503a30a393e14928062d6f77996ec
cs <acspostbox@gmail.com>
2022-12-15 18:54:54 +0530

    create dict

---
+++ 06_DICTIONARIES.py
@@ -0,0 +1,20 @@
+'''
+in python,dictionaries are used to store data values in 
+{"key" : "value"} pair formats..
+
+dictionary items are ordered,changeble,and do not allo duplicates..
+
+key must be single elements ,do not allow duplicates and allows only "string,tuple,numbers"
+
+values allow duplicates and written in "all data types"
+
+
+
+
+'''
+
+# dic={"key":"value","123":123,"name":"chandra"}
+
+# print(type(dic))
+
+
