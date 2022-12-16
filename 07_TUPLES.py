5029a9d7f9c66e4645b1b01296eb39723275fab9
cs <acspostbox@gmail.com>
2022-12-16 17:19:14 +0530

    tuple set

---
+++ 07_TUPLES.py
@@ -0,0 +1,37 @@
+# x=int(input("enter value :"))
+# print(type(x))
+# print(id(x))
+# print(len(x))
+
+
+# print("hello world!","welcome to python","this is the feature of the it",sep="*")
+
+
+'''find more index places in list'''
+# list=[1,1,1,True,False,0,78,663,11,1,1,0,8]
+
+# mi=[]
+# for i in range(len(list)):
+#     if list[i]==1:
+#         print(i)
+        
+#         # remove duplicates
+# dup=[]        
+# for i in list:
+#     if i not in dup:
+#         dup.append(i)
+# print(dup)
+
+
+'''find lenth in list'''
+# l=[1,2,3,4,5,6,7,7,8]
+# temp=0
+# for i in l:
+#     temp+=1
+# print(temp)
+# print(len(l))
+
+
+
+
+
---
+++ 08_SETS.py
@@ -0,0 +1,113 @@
+#sets
+'''
+sets are used to stored multiple items in single variable,
+
+set items are unordered, unchangeble,and un indexed,do not allow duplicated values,
+
+set items are written with curley brackets{},items are seperated with commas,
+
+set methods
+
+set.add()
+    .clear()
+    .copy()
+    .pop()
+    .remove()
+    .update()
+    
+    
+    union()
+    intersection()
+    difference()
+    symmertic-difference()
+    issubset()
+    issuperset()
+    isdisjoint()
+
+'''
+
+# thisset={1,1,2,3,4,5.5,889}
+
+# print(type(thisset))
+
+
+'''add'''
+# s={1,1,2,2,3,3,333,43,3534,(123)}
+# s.add(999)
+# print(s)
+
+'''copy'''
+# s={1,1,2,2,3,3,333,43,3534,(123)}
+# thisset=s.copy()
+# s.add("hello")
+# print(thisset)
+# print(s)
+
+'''clear'''
+# s={1,1,2,2,3,3,333,43,3534,(123)}
+# print(s.clear())
+
+'''pop'''
+# s={1,1,2,2,3,3,333,43,3534,(123)}
+# s.pop()
+# print(s)
+
+'''remove'''
+# s={1,1,2,2,3,3,333,43,3534,(123)}
+# s.remove(1)
+# print(s)
+'''update'''
+# s={1,1,2,2,3,3,333,43,3534,(123)}
+# s.update("chandranchi")
+# print(s)
+
+
+'''union'''
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# print(set1.union(set2))
+
+'''intersection'''
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# print(set1.intersection(set2))
+
+''''''
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# print(set1.difference(set2))
+
+
+'''symmertic-difference'''
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# print(set1.symmetric_difference(set2))
+
+
+'''issubset'''
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# print(set1.issubset(set2))
+
+
+# set1={1,2,3,4,5}
+# set2={1,2,3,4,5,6,7,8}
+# print(set1.issubset(set2))
+
+'''issuperset'''
+# set1={1,2,3,4,5}
+# set2={1,2,3,4,5,6,7,8}
+# print(set2.issuperset(set1))
+
+'''isdisjoint'''
+# set1={1,2,3,4,5}
+# set2={6,7,8}
+# print(set1.isdisjoint(set2))
+
+'''find lenth in list'''
+# list=[1,2,3,4,5]
+# temp=0
+# for i in list:
+#     temp+=1
+# print(temp)
+# print(len(list))
